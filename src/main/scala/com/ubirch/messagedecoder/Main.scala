96d0a3e50058750e648f3cdd5e80a32d6a98661c
Bjoern Behrens <bjoern.behrens@innoq.com>
2018-09-20 09:40:23 +0200

    clean up Main.
    UP-210

--- src/main/scala/com/ubirch/messagedecoder/Main.scala
+++ src/main/scala/com/ubirch/messagedecoder/Main.scala
@@ -1,32 +1,8 @@
 package com.ubirch.messagedecoder
 
-import akka.Done
-import akka.kafka.{ConsumerMessage, ProducerMessage, Subscriptions}
-import akka.kafka.scaladsl.Consumer.DrainingControl
-import akka.kafka.scaladsl.{Consumer, Producer}
-import akka.stream.scaladsl.Keep
-import org.apache.kafka.clients.producer.ProducerRecord
-
 object Main {
 
   def main(args: Array[String]) {
-    def transformValue(msg: ConsumerMessage.CommittableMessage[String, Array[Byte]]) = {
-      new String(msg.record.value()).toUpperCase.getBytes
-    }
-
-    val control: DrainingControl[Done] =
-      Consumer
-        .committableSource(consumerSettings, Subscriptions.topics(incomingTopic))
-        .map { msg =>
-          // ToDo BjB 19.09.18 : actual transformation should happen somewhere here
-          val transformed = transformValue(msg)
-          ProducerMessage.Message[String, Array[Byte], ConsumerMessage.CommittableOffset](
-            new ProducerRecord(outgoingTopic, transformed),
-            msg.committableOffset
-          )
-        }
-        .toMat(Producer.commitableSink(producerSettings))(Keep.both)
-        .mapMaterializedValue(DrainingControl.apply)
-        .run()
+    decoderStream.run()
   }
 }
--- src/main/scala/com/ubirch/messagedecoder/package.scala
+++ src/main/scala/com/ubirch/messagedecoder/package.scala
@@ -1,10 +1,15 @@
 package com.ubirch
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.kafka._
+import akka.kafka.scaladsl.Consumer.DrainingControl
+import akka.kafka.scaladsl.{Consumer, Producer}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Keep, RunnableGraph}
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer, StringDeserializer, StringSerializer}
 
 import scala.concurrent.ExecutionContextExecutor
@@ -36,5 +41,23 @@ package object messagedecoder {
   val incomingTopic: String = conf.getString("kafka.topic.incoming")
   val outgoingTopic: String = conf.getString("kafka.topic.outgoing")
 
+
+  def transformValue(msg: ConsumerMessage.CommittableMessage[String, Array[Byte]]) = {
+    new String(msg.record.value()).toUpperCase.getBytes
+  }
+
+  val decoderStream: RunnableGraph[DrainingControl[Done]] =
+    Consumer
+      .committableSource(consumerSettings, Subscriptions.topics(incomingTopic))
+      .map { msg =>
+        // ToDo BjB 19.09.18 : actual transformation should happen somewhere here
+        val transformed = transformValue(msg)
+        ProducerMessage.Message[String, Array[Byte], ConsumerMessage.CommittableOffset](
+          new ProducerRecord(outgoingTopic, transformed),
+          msg.committableOffset
+        )
+           }
+      .toMat(Producer.commitableSink(producerSettings))(Keep.both)
+      .mapMaterializedValue(DrainingControl.apply)
 
 }
