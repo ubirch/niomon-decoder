b45819acb2eb4d0e000dcb2d4148d3d64de89514
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-04-10 18:09:32 +0200

    refactoring

--- src/main/scala/com/ubirch/messagedecoder/MessageDecoderMicroservice.scala
+++ src/main/scala/com/ubirch/messagedecoder/MessageDecoderMicroservice.scala
@@ -10,8 +10,7 @@ import org.json4s.DefaultFormats
 import org.json4s.jackson.Serialization.write
 
 import scala.collection.mutable
-import scala.util.Try
-
+import scala.util.{Failure, Success, Try}
 import MessageDecoderMicroservice._
 
 class MessageDecoderMicroservice extends NioMicroservice[Array[Byte], String]("message-decoder") {
@@ -19,7 +18,7 @@ class MessageDecoderMicroservice extends NioMicroservice[Array[Byte], String]("m
 
   override def process(input: Array[Byte]): (String, String) = {
     transform(input) match {
-      case scala.util.Success(value) =>
+      case Success(value) =>
         logger.debug(s"decoded: $value")
 
         // signer down the line doesn't support the legacy version, so we're upgrading the version here
@@ -34,7 +33,7 @@ class MessageDecoderMicroservice extends NioMicroservice[Array[Byte], String]("m
         payload -> "valid"
 
       // TODO: maybe remove this whole section and let NioMicroservice handle all errors in a consistent way?
-      case scala.util.Failure(exception) =>
+      case Failure(exception) =>
         logger.error("error while decoding!", exception)
         val error = mutable.HashMap("error" -> exception.getMessage)
         if (exception.getCause != null) error("cause") = exception.getCause.getMessage
