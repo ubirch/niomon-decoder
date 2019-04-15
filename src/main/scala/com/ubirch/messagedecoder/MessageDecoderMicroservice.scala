799b3305d1379964ae31797c9fe5169c4ca872ea
mrobakowski <mikolaj.robakowski@ubirch.com>
2019-04-15 15:10:18 +0200

    Adjust http status code when erroring in message decoder

--- src/main/scala/com/ubirch/messagedecoder/MessageDecoderMicroservice.scala
+++ src/main/scala/com/ubirch/messagedecoder/MessageDecoderMicroservice.scala
@@ -2,20 +2,23 @@ package com.ubirch.messagedecoder
 
 import java.nio.charset.StandardCharsets
 
-import com.ubirch.kafka.{EnvelopeSerializer, MessageEnvelope}
+import com.ubirch.kafka.MessageEnvelope
 import com.ubirch.messagedecoder.MessageDecoderMicroservice._
 import com.ubirch.niomon.base.NioMicroservice
-import com.ubirch.protocol.ProtocolMessage
+import com.ubirch.niomon.base.NioMicroservice.WithHttpStatus
+import com.ubirch.protocol.{ProtocolException, ProtocolMessage}
 import com.ubirch.protocol.codec.{JSONProtocolDecoder, MsgPackProtocolDecoder}
 import org.json4s.DefaultFormats
 
 import scala.util.Try
 
-class MessageDecoderMicroservice extends NioMicroservice[Array[Byte], String]("message-decoder") {
+class MessageDecoderMicroservice extends NioMicroservice[Array[Byte], MessageEnvelope]("message-decoder") {
   implicit val formats: DefaultFormats = DefaultFormats
 
-  override def process(input: Array[Byte]): (String, String) = {
-    val value = transform(input).get
+  override def process(input: Array[Byte]): (MessageEnvelope, String) = {
+    val value = try transform(input).get catch {
+      case pe: ProtocolException => throw WithHttpStatus(400, pe)
+    }
     logger.debug(s"decoded: $value")
 
     // signer down the line doesn't support the legacy version, so we're upgrading the version here
@@ -24,10 +27,7 @@ class MessageDecoderMicroservice extends NioMicroservice[Array[Byte], String]("m
       value.setVersion((ProtocolMessage.ubirchProtocolVersion << 4) | (value.getVersion & 0x0f))
     }
 
-    val transformedEnvelope = MessageEnvelope(value)
-    val payload = EnvelopeSerializer.serializeToString(transformedEnvelope)
-
-    payload -> "valid"
+    MessageEnvelope(value) -> "valid"
   }
 }
 
